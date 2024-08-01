e6466bbb2206164707313a1cb233d59628682694
Elijah-Omotosho-OGCIO <Elijah.Omotosho@per.gov.ie>
2024-08-01 10:13:28 +0100

    Updating the Data Extractor
    - Creating a new branch called Query-Fixing
    - Updating the config.py by including the route URL for Grafana and adding the Applications apis on digital wallet and having them under the a class called 'Config'

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
-# Configuration for Prometheus URL and directories for outputs and templates
-PROMETHEUS_URL = 'https://prometheus-k8s-openshift-monitoring.apps.infomed.foma.p1.openshiftapps.com/api'
-OUTPUT_DIR = 'outputs/'
-TEMPLATE_DIR = 'templates/'
+# Configuration for Prometheus URL, Grafana URL, Applications and directories for outputs and templates under the Config Class
+class Config:
+    # Prometheus URL and Grafana URL Cluster Routes
+    PROMETHEUS_URL = "https://prometheus-k8s-openshift-monitoring.apps.infomed.foma.p1.openshiftapps.com/api"
+    GRAFANA_URL = "https://grafana-info-mediator-route-info-mediator-monitoring.apps.infomed.foma.p1.openshiftapps.com"
+
+    # Applications to monitor and their respective dashboards
+    APPLICATIONS = ["birth-api","driving-license-api"]
+
+    # Output Directories
+    OUTPUT_DIR = 'outputs/'
+    TEMPLATE_DIR = 'templates/'
